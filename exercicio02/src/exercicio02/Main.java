530b49e1ae98c91fbdbe4ef0ebd6dae71238a23d
PIERSILVIO DE CARVALHO ORLANDINI <156609726+orlandini01@users.noreply.github.com>
2024-12-04 13:32:45 -0300

    Update Main.java

--- exercicio02/src/exercicio02/Main.java
+++ exercicio02/src/exercicio02/Main.java
@@ -1,3 +1,16 @@
+/*Construa um projeto em Java que usa o padrão JavaBean para representar um produto. O código envolve a criação de uma classe Produto seguindo o padrão JavaBean, com atributos privados, métodos getter e setter, e um método na classe principal para ler os dados e exibir o resultado.
+
+Descrição do Exercício
+
+Crie uma classe Produto que segue o padrão JavaBean:
+Atributos: nome, quantidade e preço unitário.
+Métodos getter e setter para cada atributo.
+Na classe principal, use a classe Scanner para capturar os dados do usuário, preencha os atributos do objeto Produto e calcule o valor total da compra. Exiba uma mensagem com o nome do produto e o valor total.
+
+Estrutura do Projeto
+Classe Produto (JavaBean)
+Classe Main para execução e interação com o usuário.
+*/
 package exercicio02;
 
 import java.util.Scanner;
